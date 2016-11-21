75094267421998db9ed2fc8d0b51746ef6eacdb5
Sheila <sheilafatanihzasafitri@gmail.com>
2016-11-21 09:35:19 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
@@ -33,6 +33,17 @@ public class InputActivity extends AppCompatActivity {
         etLokasi = (EditText) findViewById(R.id.editTextLokasi);
         ivFoto = (ImageView) findViewById(R.id.imageViewFoto);
 
+        hotel = (Hotel) getIntent().getSerializableExtra(MainActivity.HOTEL);
+        if (hotel!=null)
+        {
+            setTitle("Edit "+hotel.judul);
+            fillData();
+        }
+        else
+        {
+            setTitle("New Hotel");
+        }
+
         ivFoto.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -53,6 +64,15 @@ public class InputActivity extends AppCompatActivity {
                 }
                 ));
 
+    }
+
+    private void fillData() {
+        etJudul.setText(hotel.judul);
+        etDeskripsi.setText(hotel.deskripsi);
+        etDetail.setText(hotel.detail);
+        etLokasi.setText(hotel.lokasi);
+        uriFoto = Uri.parse(hotel.foto);
+        ivFoto.setImageURI(uriFoto);
     }
 
     private void pickPhoto() {
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -21,11 +21,13 @@ import java.util.ArrayList;
 import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
-public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter{
+public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
     public static final String HOTEL = "hotel";
     public static final int REQUEST_CODE_ADD = 88;
+    public static final int REQUEST_CODE_EDIT = 99;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+    int itemPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +112,29 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     }
 
     @Override
+    public void doEdit(int pos) {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int pos) {
+
+    }
+
+    @Override
+    public void doShare(int pos) {
+
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
@@ -117,6 +142,13 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         {
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
+        else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK)
+        {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos, hotel);
             mAdapter.notifyDataSetChanged();
         }
     }
